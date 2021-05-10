fbce76ff3b7767f82041598de86467df98b9a788
Layesh <layesh.iut@gmail.com>
2021-05-10 15:09:36 +0600

    Added users module (register, login, profile).

--- taxlover/admin.py
+++ taxlover/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from taxlover.models import TaxPayer, Salary
+from taxlover.models import TaxPayer, Salary, Division, District
 
 admin.site.register(TaxPayer)
 admin.site.register(Salary)
+admin.site.register(Division)
+admin.site.register(District)
