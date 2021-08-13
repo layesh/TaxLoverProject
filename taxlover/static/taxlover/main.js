33769076dc164c26d5133a786e9ab82cebf8df3e
Layesh <layesh.iut@gmail.com>
2021-08-13 08:17:46 +0600

    Made Dropdown menu visible on hover.

--- taxlover/static/taxlover/main.js
+++ taxlover/static/taxlover/main.js
@@ -7,4 +7,13 @@ $(function () {
     });
 });
 
+$(document).ready(function(){
+    $(".dropdown").hover(function(){
+        var dropdownMenu = $(this).children(".dropdown-menu");
+        if(dropdownMenu.is(":visible")){
+            dropdownMenu.parent().toggleClass("open");
+        }
+    });
+});
+
 
